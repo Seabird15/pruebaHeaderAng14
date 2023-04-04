4c10ff038880eaf82e8a5944946162ba2476c189
Daniela Lizama <2b-0005@MacBook-Pro-de-IT.local>
2023-04-04 12:45:07 -0400

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { DataTableComponentComponent } from './data.table-component/data.table-component.component';
+import { BannerPortadaComponentComponent } from './banner.portada-component/banner.portada-component.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    BannerPortadaComponentComponent,
+    DataTableComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
